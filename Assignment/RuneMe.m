190795d354df464d56001a2fc3a5274ec5092837
endysis <shea_o@me.com>
2016-11-02 10:43:28 +0000

    Ass

--- Assignment/RuneMe.m
+++ Assignment/RuneMe.m
@@ -13,7 +13,7 @@ IG = rgb2gray(I);  % Can I use this? IG == Image Gray
 %figure;
 %imshow(IG);
 %title('Step-2: Conversion of input image to greyscale');
-
+%{
 % Step-3: Noise removal - Median Filter - To remove Salt and Pepper Noise
 x = size(IG,1); % Size of X Coordinates
 y = size(IG,2); % Size of Y Coordinates
@@ -36,7 +36,51 @@ for i = 2:size(padI,1)-1   % Copying over the gray image to the padded matrix
 end
 
 imshow(padI);
-%imshow(IG);
+%imshow(IG); %}
+
+%}
+
+x = size(IG,1);
+y = size(IG,2);
+
+IG = padarray(IG,[1,1]);
+
+IG = medfilt2(IG);
+
+IGW = zeros(size(IG));
+
+
+x1 = 1;
+y1 = 1;
+
+for i = 2:size(IG,1)-1
+    y1 = 1;
+    for j = 2:size(IG,2)-1
+    IGW(x1,y1) = IG(i,j);
+    y1 = y1 + 1;
+    end
+    x1 = x1 + 1;
+end
+
+
+
+imshow(IGW);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
